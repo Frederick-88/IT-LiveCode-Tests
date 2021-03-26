59da114a64f7661547f41b3e618475a13ad33cea
Chen Frederick <frederick@offeo.com>
2021-03-26 19:52:11 +0700

    Added HardMultiply Javascript Live Code Test

---
+++ HardMultiply/index.js
@@ -0,0 +1,15 @@
+function multiply(a) {
+  // code here
+}
+
+console.log(multiply(5)(6)); // expected result : 30
+console.log(multiply(8)(2)); // expected result : 16
+console.log(multiply(10)(10)); // expected result : 100
+console.log(multiply(12)(15)); // expected result : 180
+
+// answer
+// function multiply(a) {
+//   return function(b) {
+//     return a * b;
+//   }
+// }
