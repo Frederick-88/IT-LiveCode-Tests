eac5a1f77ffbcecf80415c3df1c60bcef46226be
Chen Frederick <frederickfd88@gmail.com>
2021-05-08 17:25:01 +0700

    HardMultiply Update

--- HardMultiply/index.js
+++ HardMultiply/index.js
@@ -1,5 +1,9 @@
 function multiply(a) {
   // code here
+  // answer below
+  return function (b) {
+    return a * b;
+  };
 }
 
 const Test = (testcase, result) => console.log(testcase === result);
@@ -8,10 +12,3 @@ Test(multiply(5)(6), 30); // expected result : 30
 Test(multiply(8)(2), 16); // expected result : 16
 Test(multiply(10)(10), 100); // expected result : 100
 Test(multiply(15)(12), 180); // expected result : 180
-
-// answer
-// function multiply(a) {
-//   return function(b) {
-//     return a * b;
-//   }
-// }
