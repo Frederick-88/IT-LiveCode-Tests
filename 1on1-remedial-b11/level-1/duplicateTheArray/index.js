ccdfed90997eabeb07c5fa0fad2efc4e087f4eab
Chen Frederick <frederickfd88@gmail.com>
2021-05-10 19:32:33 +0700

    Added Remedial Code Tests & Fixed Code Tester

--- 1on1-remedial-b11/level-1/duplicateTheArray/index.js
+++ 1on1-remedial-b11/level-1/duplicateTheArray/index.js
@@ -1,5 +1,6 @@
 function duplicate(array) {
   // code here
+  return array.concat(array);
 }
 
 // do not change this code below
@@ -32,8 +33,3 @@ test(duplicate(["new", "day", "is", "good"]), [
 ]);
 test(duplicate([3, 2, 7, 5, 6]), [3, 2, 7, 5, 6, 3, 2, 7, 5, 6]);
 test(duplicate([1]), [1, 1]);
-
-// // Answer
-// function duplicate(array) {
-//   return array.concat(array);
-// }
---
+++ 1on1-remedial-b11/level-1/highestInteger/index.js
@@ -0,0 +1,28 @@
+const find = (input) => {
+  //write your code here
+  let max,
+    result = "";
+  try {
+    let arr = input.split(" ").map((x) => +x);
+    max = Math.max(...arr);
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== max) {
+        result += `${Math.min(...arr.filter((x) => x > arr[i]))} `;
+      } else {
+        result += `^ `;
+      }
+    }
+    return result;
+  } finally {
+    result = "";
+  }
+};
+
+const test = (testCase, result) => {
+  if (testCase !== result) return console.log(false);
+  return console.log(true);
+};
+
+test(find("10 9 2 5 1 2 4 0 56"), "56 10 4 9 2 4 5 1 ^ ");
+test(find("3 8 99 3 2 44 12"), "8 12 ^ 8 3 99 44");
